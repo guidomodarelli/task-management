77f431ebb2b69e78de926fde2d4a5e22517cfd00
guidomodarelli25 <guidomodarelli25@gmail.com>
2024-05-11 16:50:19 -0300

    style: Update validation decorators in GetTasksFilterDto

--- src/tasks/dto/get-tasks-filter.dto.ts
+++ src/tasks/dto/get-tasks-filter.dto.ts
@@ -1,7 +1,13 @@
+import { IsEnum, IsOptional, IsString } from "class-validator";
 import { TaskStatus } from "../task.model";
 
 export class GetTasksFilterDto {
+  @IsEnum(TaskStatus)
+  @IsOptional()
   status?: TaskStatus;
+
+  @IsOptional()
+  @IsString()
   search?: string;
 }
 
