3631f31bd39f78603e6c161a9766e788c9b1760d
guidomodarelli25 <guidomodarelli25@gmail.com>
2024-05-12 01:24:32 -0300

    fix: Add unique constraint to username field and hash password

--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -1,21 +1,40 @@
 import { DataSource, Repository } from 'typeorm';
 import { User } from './user.entity';
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsersRepository extends Repository<User> {
+  private readonly DUPLICATED_CODE = 23505;
+
   constructor(dataSource: DataSource) {
     super(User, dataSource.createEntityManager());
   }
 
   async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     const { username, password } = authCredentialsDto;
-    
+
+    // hash
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+
     const user = this.create({
       username,
-      password
+      password: hashedPassword,
     });
-    await this.save(user);
+
+    try {
+      await this.save(user);
+    } catch (error) {
+      if (Number(error.code) === this.DUPLICATED_CODE) {
+        throw new ConflictException('username already exists');
+      }
+      throw new InternalServerErrorException();
+    }
   }
 }
