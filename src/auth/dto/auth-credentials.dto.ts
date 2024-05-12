f0b38443a767e48584d55be22acb4db4c85fe3d3
guidomodarelli25 <guidomodarelli25@gmail.com>
2024-05-12 00:42:32 -0300

    feat: Add signUp endpoint to AuthController and createUser method to UsersRepository

---
+++ src/auth/dto/auth-credentials.dto.ts
@@ -0,0 +1,19 @@
+import { IsString, IsStrongPassword, Length, Matches } from 'class-validator';
+
+export class AuthCredentialsDto {
+  @IsString()
+  @Length(4, 28)
+  username: string;
+
+  @IsString()
+  @Length(8, 32)
+  @IsStrongPassword({
+    minLowercase: 1,
+    minSymbols: 1,
+    minNumbers: 1,
+    minUppercase: 1,
+  }, {
+      message: 'password must contain at least 1 uppercase letter, 1 lowercase letter, 1 number, and 1 special character'
+    })
+  password: string;
+}
--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -1,12 +1,21 @@
-import { DataSource, Repository } from "typeorm";
-import { User } from "./user.entity";
-import { Injectable } from "@nestjs/common";
+import { DataSource, Repository } from 'typeorm';
+import { User } from './user.entity';
+import { Injectable } from '@nestjs/common';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 
 @Injectable()
 export class UsersRepository extends Repository<User> {
-
   constructor(dataSource: DataSource) {
-    super(User, dataSource.createEntityManager())
+    super(User, dataSource.createEntityManager());
   }
 
+  async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    const { username, password } = authCredentialsDto;
+    
+    const user = this.create({
+      username,
+      password
+    });
+    await this.save(user);
+  }
 }
