7028b3d639a89c9c18009e09398394a8194a80a7
guidomodarelli25 <guidomodarelli25@gmail.com>
2024-05-12 18:12:21 -0300

    feat: Add configuration module for multiple stages

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: [`.env.stage.${process.env.STAGE}`],
+    }),
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: 'localhost',
@@ -16,7 +20,7 @@ import { AuthModule } from './auth/auth.module';
       synchronize: true,
     }),
     TasksModule,
-    AuthModule
+    AuthModule,
   ],
 })
 export class AppModule {}
