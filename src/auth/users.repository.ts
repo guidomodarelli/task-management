42bea030866334d7c0f4dbf5c8d83ffbc6293470
guidomodarelli25 <guidomodarelli25@gmail.com>
2024-05-12 00:07:11 -0300

    feat: Add authentication module with controller, service, entity, and repository

---
+++ src/auth/users.repository.ts
@@ -0,0 +1,12 @@
+import { DataSource, Repository } from "typeorm";
+import { User } from "./user.entity";
+import { Injectable } from "@nestjs/common";
+
+@Injectable()
+export class UsersRepository extends Repository<User> {
+
+  constructor(dataSource: DataSource) {
+    super(User, dataSource.createEntityManager())
+  }
+
+}
